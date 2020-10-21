c12edb5ae6387fd5791788d379b6b22c17759110
nicke <nicken333@gmail.com>
2020-10-21 15:20:26 +0200

    Tagit bort kommentarer.

--- Library.java
+++ Library.java
@@ -1,20 +1,14 @@
 public class Library {
 
-	/**
-	 * 
-	 * @param id
-	 */
+
 	private boolean parseId(int id) {
-		// TODO - implement Library.parseId
+
 		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * 
-	 * @param id
-	 */
+
 	public void writeProduct(int id) {
-		// TODO - implement Library.writeProduct
+
 		throw new UnsupportedOperationException();
 	}
 
