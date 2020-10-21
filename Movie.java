d6ce8bbc4b822e99b7618801e7b07b988d0e6563
nicke <nicken333@gmail.com>
2020-10-21 14:57:27 +0200

    Tagit bort kommentarer.

--- Movie.java
+++ Movie.java
@@ -8,27 +8,15 @@ public class Movie {
 	private int length;
 	private float rating;
 
-	/**
-	 * 
-	 * @param id
-	 * @param title
-	 * @param value
-	 * @param borrowed
-	 */
+
 	public void product(int id, String title, int value, boolean borrowed) {
-		// TODO - implement Movie.product
 		this.id = id;
 		this.title = title;
 		this.value = value;
 	}
 
-	/**
-	 * 
-	 * @param length
-	 * @param rating
-	 */
 	public void update(int length, float rating) {
-		// TODO - implement Movie.update
+
 		throw new UnsupportedOperationException();
 	}
 
