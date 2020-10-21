6508eee1df073cd1d7b5640bd92eac98a827d144
nicke <nicken333@gmail.com>
2020-10-21 15:37:36 +0200

    Constructor for LibraryManager Constructor for Library
    Added main() in LibraryManager

--- Library.java
+++ Library.java
@@ -1,4 +1,16 @@
-public class Library {
+import java.io.FileNotFoundException;
+
+public class Library implements IntLib {
+
+	//private Book[] books;
+	//private Movie[] movies;
+	private String productPath;
+
+	public Library (String productPath) throws FileNotFoundException {
+		this.productPath = productPath;
+		//books = parseBooks(productPath);  //not sure if this is right
+		//movies = parseMovies(productPath);
+	}
 
 
 	private boolean parseId(int id) {
@@ -12,4 +24,13 @@ public class Library {
 		throw new UnsupportedOperationException();
 	}
 
+	@Override
+	public void register() {
+
+	}
+
+	@Override
+	public void deregister() {
+
+	}
 }
